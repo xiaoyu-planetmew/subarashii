264874a39d57ee846b76fc982593c713272d4131
xzxthu <xzxvg@sina.com>
2022-04-23 23:12:24 +0900

    Realized art materials

---
+++ Assets/Effects/GradientLine/GradientLineControl.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GradientLineControl : MonoBehaviour
+{
+    private LineRenderer line;
+    public List<Transform> dots = new List<Transform>();
+
+    private void Start()
+    {
+        line = gameObject.GetComponent<LineRenderer>();
+
+        foreach (Transform child in this.transform)
+        {
+            dots.Add(child);
+            child.GetComponent<SpriteRenderer>().enabled = false;
+        }
+
+        line.positionCount = dots.Count;
+
+        for (int i = 0; i < dots.Count; i++)
+        {
+            line.SetPosition(i, dots[i].position);
+        }
+    }
+}
