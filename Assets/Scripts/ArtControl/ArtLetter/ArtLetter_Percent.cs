8e4fd55c2eba7e1a4cd5867ccd52662330093f9b
xiaoyu-planetmew <xiaoyu_planetmew@hotmail.com>
2022-05-01 01:24:08 +0800

    artletter

--- Assets/Scripts/ArtControl/ArtLetter/ArtLetter_Percent.cs
+++ Assets/Scripts/ArtControl/ArtLetter/ArtLetter_Percent.cs
@@ -6,96 +6,24 @@ using UnityEngine.UI;
 
 public class ArtLetter_Percent : MonoBehaviour
 {
-    public bool ShowZero = true;
-    public Texture[] letters;
-    public Texture dot;
-    public Texture percent;
-    public int num = 6;
-    [Range(0,1)] public float lettersSize = 1;
-    public float wordSpace;
-    [SerializeField] private int showNumber;
-    private int storedNumber;
-    
-    private RawImage[] images;
-    private float localScaleX = 0.61f;
-    private Animator[] anims;
-    private bool startAddEffect = false;
-
-    private void Start()
-    {
-        InitialNumbers();
-        anims = GetComponentsInChildren<Animator>();
-        showNumber = 0;
-        storedNumber = 0;
-        startAddEffect = false;
-    }
-
-    private void Update()
-    {
-        //SetLetters(lettersSize, wordSpace);
-        //UpdateShowLetters();
-
-        if(startAddEffect)
-        {
-            if(showNumber>=storedNumber)
-            {
-                showNumber = storedNumber;
-                startAddEffect = false;
-                //Debug.Log("到了");
-                UpdateShowLetters();
-                return;
-            }
-
-            showNumber += Mathf.Max(((int)(storedNumber-showNumber)/20),1);
-            UpdateShowLetters();
-        }
-    }
-
-    private void InitialNumbers()
-    {
-        
-        images = new RawImage[num];
-        for(int i = 0;i<num;i++)
-        {
-            GameObject obj = Instantiate(Resources.Load<GameObject>("Prefabs/ArtLetter"));
-            images[i] = obj.GetComponent<RawImage>();
-            obj.transform.SetParent(transform);
-            obj.GetComponent<RectTransform>().localPosition = Vector3.left * (wordSpace) * i;
-            obj.GetComponent<RectTransform>().localScale = new Vector3(localScaleX * lettersSize, lettersSize, 1);
-        }
-    }
-
-    /// <summary>
-    /// 更新显示的数字
-    /// </summary>
+    public GameObject letter;
+    public GameObject letter1;
     public void UpdateShowLetters()
     {
-        int howLong = Mathf.FloorToInt(Mathf.Log10(showNumber))+1;
-        //Debug.Log(howLong);
-
-        for (int i = 0; i < num; i++)
-        {
-            images[i].texture = letters[Mathf.FloorToInt((showNumber / Mathf.Pow(10, i))) % 10];
-
-            if(!ShowZero)
-            {
-                images[i].enabled = !(i > howLong - 1) ;
-            }
-        }
+        letter.GetComponent<ArtLetter>().UpdateShowLetters();
+        letter1.GetComponent<ArtLetter>().UpdateShowLetters();
     }
 
-    public void SetShowNumberWithEffect(int num)
+    public void SetShowNumberWithEffect(float num)
     {
-        storedNumber = num;
-        startAddEffect = true;
+        letter.GetComponent<ArtLetter>().SetShowNumberWithEffect(Mathf.FloorToInt(((num * 10000) / Mathf.Pow(10, 3))) % 10 * 10 + Mathf.FloorToInt(((num * 10000) / Mathf.Pow(10, 2))) % 10);
+        letter1.GetComponent<ArtLetter>().SetShowNumberWithEffect(Mathf.FloorToInt(((num * 10000) / Mathf.Pow(10, 1))) % 10 * 10 + Mathf.FloorToInt(((num * 10000) / Mathf.Pow(10, 0))) % 10);
     }
 
-    public void SetShowNumber(int num)
+    public void SetShowNumber(float num)
     {
-        
-        showNumber = num;
-        UpdateShowLetters();
-
+        letter.GetComponent<ArtLetter>().SetShowNumber(Mathf.FloorToInt(((num * 10000) / Mathf.Pow(10, 3))) % 10 * 10 + Mathf.FloorToInt(((num * 10000) / Mathf.Pow(10, 2))) % 10);
+        letter1.GetComponent<ArtLetter>().SetShowNumber(Mathf.FloorToInt(((num * 10000) / Mathf.Pow(10, 1))) % 10 * 10 + Mathf.FloorToInt(((num * 10000) / Mathf.Pow(10, 0))) % 10);
     }
 
     /// <summary>
@@ -105,34 +33,25 @@ public class ArtLetter_Percent : MonoBehaviour
     /// <param name="space"></param>
     public void SetLetters(float size, float space)
     {
-        for (int i = 0; i < num; i++)
-        {
-            images[i].gameObject.GetComponent<RectTransform>().localPosition = Vector3.left * (wordSpace) * i;
-            images[i].gameObject.GetComponent<RectTransform>().localScale = new Vector3(localScaleX * lettersSize, lettersSize, 1);
-        }
+        letter.GetComponent<ArtLetter>().SetLetters(size, space);
+        letter1.GetComponent<ArtLetter>().SetLetters(size, space);
     }    
 
     public void Blink()
     {
-        foreach(Animator anim in anims)
-        {
-            anim.SetTrigger("Blink");
-        }
+        letter.GetComponent<ArtLetter>().Blink();
+        letter1.GetComponent<ArtLetter>().Blink();
     }
 
     public void StartKeepBlink()
     {
-        foreach (Animator anim in anims)
-        {
-            anim.SetBool("KeepBlink", true);
-        }
+        letter.GetComponent<ArtLetter>().StartKeepBlink();
+        letter1.GetComponent<ArtLetter>().StartKeepBlink();
     }
 
     public void StopKeepBlink()
     {
-        foreach (Animator anim in anims)
-        {
-            anim.SetBool("KeepBlink", false);
-        }
+        letter.GetComponent<ArtLetter>().StopKeepBlink();
+        letter1.GetComponent<ArtLetter>().StopKeepBlink();
     }
 }
