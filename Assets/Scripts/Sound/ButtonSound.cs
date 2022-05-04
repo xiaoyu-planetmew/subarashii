08fb71362f01970c270bb6c8be215b6592af0e7b
xzxthu <xzxvg@sina.com>
2022-05-04 18:50:03 +0800

    add button sound

---
+++ Assets/Scripts/Sound/ButtonSound.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonSound : MonoBehaviour, IPointerEnterHandler
+{
+    public AkEvent HoverSound;
+    public AkEvent ClickSound;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (HoverSound != null)
+            HoverSound.HandleEvent(WwiseManager.Instance.gameObject);
+    }
+
+    public void PlayClickSound()
+    {
+        ClickSound.HandleEvent(WwiseManager.Instance.gameObject);
+    }
+    
+}
