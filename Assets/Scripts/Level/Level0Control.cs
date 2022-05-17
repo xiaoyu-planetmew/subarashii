bdc47480dab09efbe63af10ae91099ba4c9afdf3
xiaoyu-planetmew <xiaoyu_planetmew@hotmail.com>
2022-05-18 00:19:54 +0800

    lv0

--- Assets/Scripts/Level/Level0Control.cs
+++ Assets/Scripts/Level/Level0Control.cs
@@ -8,6 +8,8 @@ public class Level0Control : MonoBehaviour
     bool openFinished = false;
 
     public int pressNum;
+    int hitCount = 0;
+    [SerializeField] bool isShaking = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +19,17 @@ public class Level0Control : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKey(KeyCode.S))
+        {
+            if(hitCount >= 3)
+            {
+                CapsuleAnim2();
+                StopAllCoroutines();
+                StartCoroutine(Open());
+            }
+            if(!isShaking && hitCount < 3) StartCoroutine(Shake());
 
+        }
 /*        if(Input.anyKeyDown && !openFinished)
         {
             if(pressNum < 3)
@@ -49,5 +61,25 @@ public class Level0Control : MonoBehaviour
     public void dialog2Start()
     {
         DialogSys.Instance.dialogStart(1);
+    }
+    public void OpenCapsule()
+    {
+        isShaking = false;
+    }
+    IEnumerator Shake()
+    {
+        isShaking = true;
+        CapsuleAnim1();
+        yield return new WaitForSeconds(0.5f);
+        hitCount++;
+        if(hitCount < 3)
+        isShaking = false;
+        
+    }
+    IEnumerator Open()
+    {
+        yield return new WaitForSeconds(2f);
+        SceneController.Instance.ChangeScene("Level_1_ver4");
+        Time.timeScale = 1;
     }
 }
